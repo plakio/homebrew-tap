603e97a461b6ba9692dc9b87a45f2cc43adb227d
Oscar Hugo Paz <oscarhugopazmariscal@gmail.com>
2026-06-30 14:40:24 -0500

    plak-cli 0.4.47

--- Formula/plak-cli.rb
+++ Formula/plak-cli.rb
@@ -1,8 +1,8 @@
 class PlakCli < Formula
   desc "Interactive Bash CLI for SSH servers, local domains, and SSH keys"
   homepage "https://github.com/plakio/plak-cli"
-  url "https://github.com/plakio/plak-cli/archive/refs/tags/v0.4.46.tar.gz"
-  sha256 "855c6daaf60496310fac2654f5281be10fc72dc5362865c838241ec06ca74d6a"
+  url "https://github.com/plakio/plak-cli/archive/refs/tags/v0.4.47.tar.gz"
+  sha256 "3e3e371fea7a90b10cd1b5cfee9917adeb3fe609bb10ada67608544ab828202a"
   license "MIT"
 
   depends_on "gum"
@@ -13,7 +13,7 @@ class PlakCli < Formula
   end
 
   test do
-    assert_match "plak v0.4.46", shell_output("#{bin}/plak version")
+    assert_match "plak v0.4.47", shell_output("#{bin}/plak version")
     assert_match "Dependencies:", shell_output("#{bin}/plak status")
   end
 end
